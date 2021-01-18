fd1d447c75d38c792a411a32785d7a1f8625dc8f
dk <dk@test.com>
2021-01-18 18:10:39 +0800

    Import Antd

---
+++ craco.config.js
@@ -0,0 +1,57 @@
+const CracoAntDesignPlugin = require("craco-antd");
+const CracoAlias = require("craco-alias");
+const CracoLessPlugin = require("craco-less");
+ 
+const path = require('path');
+
+module.exports = {
+    plugins: [
+      /* antd组件按需加载&样式覆盖等 */
+      {
+        plugin: CracoAntDesignPlugin,
+        options: {
+          customizeThemeLessPath: path.join(
+            __dirname,
+            "src/styles/antd.theme.less"
+          ),
+        },
+      },
+      /* 支持less module */
+      {
+        plugin: CracoLessPlugin,
+        options: {
+          cssLoaderOptions: {
+            modules: { localIdentName: "[local]_[hash:base64:5]" },
+          },
+          modifyLessRule: function(lessRule, _context) {
+            lessRule.test = /\.(module)\.(less)$/;
+            lessRule.exclude = path.join(__dirname, 'node_modules');
+            return lessRule;
+          },
+        },
+      },
+      /* 别名设置 */
+      {
+        plugin: CracoAlias,
+        options: {
+          source: "tsconfig",
+          baseUrl: "./src",
+          tsConfigPath: "./tsconfig.extend.json"
+        }
+      },
+    ],
+    devServer: (devServerConfig) => {
+      return {
+        ...devServerConfig,
+        // 服务开启gzip
+        compress: true,
+        proxy: {
+          '/api': {
+            target: 'https://test.com/',
+            changeOrigin: true,
+            xfwd: false,
+          }
+        }
+      }
+    }
+  };
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import { Button, ConfigProvider, DatePicker, message } from 'antd';
+// 由于 antd 组件的默认文案是英文，所以需要修改为中文
+import zhCN from 'antd/lib/locale/zh_CN';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+import 'antd/dist/antd.css';
 
-function App() {
+moment.locale('zh-cn');
+
+const App = () => {
   return (
-    <div className="App">
-      React Antd Admin Template
-    </div>
+    <ConfigProvider locale={zhCN}>
+      <Button type="primary">Button</Button>
+    </ConfigProvider>
   );
-}
+};
 
 export default App;
